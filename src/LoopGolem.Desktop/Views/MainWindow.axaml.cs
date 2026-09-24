401535e4593a4386bc26b1dbecb1d288ea612f62
pedromartinscs <pedro@nancode.com.br>
2026-09-24 10:36:16 -0300

    fix: import worker protocol response

--- src/LoopGolem.Desktop/Views/MainWindow.axaml.cs
+++ src/LoopGolem.Desktop/Views/MainWindow.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
 using LoopGolem.Core.Domain;
+using LoopGolem.Core.Protocol;
 using LoopGolem.Desktop.Ipc;
 using LoopGolem.Desktop.Localization;
 
